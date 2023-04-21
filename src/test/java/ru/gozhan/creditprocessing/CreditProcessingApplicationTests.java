d79d0e4b93842e11c98548552902f9a413b6b4d0
Sanchellik <sashagozhan@gmail.com>
2023-04-21 20:30:48 +0300

    init

---
+++ src/test/java/ru/gozhan/creditprocessing/CreditProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gozhan.creditprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

567ec6ec3da5a42995142c319304e3a85ce0759c
Sanchellik <sashagozhan@gmail.com>
2023-04-27 02:08:50 +0300

    from this attempts to refactor security

--- src/main/java/ru/gozhan/loanservice/security/UserDetailsServiceImpl.java
+++ src/main/java/ru/gozhan/loanservice/security/UserDetailsServiceImpl.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import ru.gozhan.loanservice.repository.UserRepository;
 
 import java.util.Optional;
 
-@Component
+@Service
 //@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
@@ -18,8 +18,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> userInfo = userRepository.findByUsername(username);
-        return userInfo.map(UserDetailsImpl::new)
+        Optional<User> user = userRepository.findByUsername(username);
+        return user.map(UserDetailsImpl::new)
                 .orElseThrow(() -> new UsernameNotFoundException("user not found " + username));
 
     }
